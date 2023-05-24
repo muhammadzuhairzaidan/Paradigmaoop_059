2efacd601c6026b7ed6fa262421d799cead8f09b
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-24 14:10:48 +0700

    member public class dokter dan class pasien

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 class dokter;
 class pasien {
-
+public:
+	string nama;
+	vector<dokter*> daftar_dokter;
+	pasien(string pNama) :nama(pNama) {
+		cout << "Pasien \" " << nama << " \" ada\n";
+	}
+	~pasien() {
+		cout << "pasien \"" << nama << "\" tidak ada\n";
+	}
+	void tambahDokter(dokter*);
+	void cetakDokter();
 };
+

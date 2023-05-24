384d7b12e5e78bb10aa8b79493c4511abc190c44
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-24 11:17:02 +0700

    menambahkan member publik pada orang

--- paradigma_oop/paradigma_oop.cpp
+++ paradigma_oop/paradigma_oop.cpp
@@ -3,4 +3,18 @@
 using namespace std;
 
 class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+	int jumlah(int a, int b) {
+		return a + b;
+	}
 };

f5bedf30a664ff5ea08c33efd309a71c64c5f606
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-24 19:55:17 +0700

    membuat header jantung

---
+++ MainJM/jantung.h
@@ -0,0 +1,14 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+using namespace std;
+
+class jantung {
+public:
+	jantung() {
+		cout << "jantung dihidupkan\n";
+	}
+	~jantung() {
+		cout << "jantung dimatikan\n";
+	}
+};
+#endif

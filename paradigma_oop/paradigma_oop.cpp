08a1afa3b20a57edb1632fa54a996476ea965f39
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-24 11:33:41 +0700

    untuk memanggil prosedur

--- paradigma_oop/paradigma_oop.cpp
+++ paradigma_oop/paradigma_oop.cpp
@@ -38,3 +38,9 @@ public:
 		return "Halo, nama saya " + nama + "dari sekolah " + "\n\n";
 		}
 };
+int main() {
+	pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+	cout << siswa1.perkenalan();
+	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+	return 0;
+}
